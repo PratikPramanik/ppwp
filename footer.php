463ff2512c9aa47b2ec00a8b1d9f93848b7d1547
unknown <Sandwich@Sandwich-PC.(none)>
2012-03-13 15:05:15 -0700

    Initial commit

---
+++ footer.php
@@ -0,0 +1,12 @@
+<hr />
+<div id="footer" class="grid_16">
+	<p>
+		&copy; <?php echo date('Y'); ?>. <?php bloginfo('name'); ?>. Powered by
+		<a href="http://wordpress.org/">WordPress</a>. Theme design by <a href="http://wpshoppe.com/">WPShoppe</a>.
+		<li class="last-child"><a href="<?php bloginfo('rss2_url'); ?>" class="rss">RSS</a></li>
+	</p>
+</div>
+</div>
+		<?php wp_footer(); ?>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes(); ?>>
+
+<head profile="http://gmpg.org/xfn/11">
+<meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
+
+<title><?php wp_title('&laquo;', true, 'right'); ?> <?php bloginfo('name'); ?></title>
+
+<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
+<!--[if IE]><link type="text/css" href="<?php bloginfo('template_directory'); ?>/css/ie.css" rel="stylesheet" media="all" /><![endif]-->
+<link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> RSS Feed" href="<?php bloginfo('rss2_url'); ?>" />
+<link rel="alternate" type="application/atom+xml" title="<?php bloginfo('name'); ?> Atom Feed" href="<?php bloginfo('atom_url'); ?>" />
+<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+
+<?php wp_head(); ?>
+
+</head>
+<body>
+<div class="wrapper"> <!--end in Footer.php-->
+
+	<div class="container">
+		<!-- TITLE BAR AT VERY TOP -->
+		<div class="row">
+			<div class="threecol">
+				<!--<p>Three columns</p>-->
+			</div>
+			
+			<div id="header" class="sixcol">
+				 <h1><?php bloginfo('description'); ?><a href="<?php echo get_option('home'); ?>/"><?php bloginfo('name'); ?></a><span class="description"></span></h1>	
+
+				<!--<ul id="nav">
+				  <?php #wp_list_pages('title_li=Pages'); ?>
+				  <?php #wp_list_categories('title_li=Categories'); ?>
+				</ul> -->
+		  
+			</div>
+			
+			<div class="threecol last">
+				<!--<p>Three columns</p>-->
+			</div>
+		</div>
+		
+		<!-- NAV BAR ROW -->
+		<div class="row">
+			<div class="threecol">
+				<!--<p>Three columns</p>-->
+			</div>
+			
+			<div id="navbar" class="sixcol" >
+				<a>Portfolio</a>
+				<a>Papers</a>
+				<a>Artwork</a>
+				<a>Blog</a>
+				<a>Home/Root/Profile</a>
+			</div>
+			
+			<div class="threecol last">
+				<!--<p>Three columns</p>-->
+			</div>
+		</div>
+	</div>
+<hr />
