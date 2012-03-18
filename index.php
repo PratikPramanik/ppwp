f3b628fa1913047054df8ad0dd06201a89ccbf64
unknown <Sandwich@Sandwich-PC.(none)>
2012-03-17 23:22:42 -0700

    delete dedicate pages

--- index.php
+++ index.php
@@ -8,35 +8,34 @@
 			<!--EMPTY -->
 		</div>
 		
-		<div class="eightcol">
+		<div class="eightcol content-area">
 			<?php if (have_posts()) : ?>
-			
-			
-			<?php while (have_posts()) : the_post(); ?>
 
-				<div <?php post_class() ?> id="post-<?php the_ID(); ?>">
-				<span class="postmetadata"><?php the_category(' / ') ?> &mdash; <?php edit_post_link('Edit', '', ' &mdash; '); ?>  <?php comments_popup_link('No comments', '1 comment', '% comments'); ?></span><br/>
-					<small><span class="date"><?php the_time('d') ?></span><br /><?php the_time('M y') ?> <!-- by <?php the_author() ?> --></small>
-					<h2><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
-					
-					
+				<?php while (have_posts()) : the_post(); ?>
+					<h2><?php the_title(); ?></h2>
+					<div <?php post_class() ?> id="post-<?php the_ID(); ?>">
+					<span class="postmetadata"><?php the_category(' / ') ?> &mdash; <?php edit_post_link('Edit', '', ' &mdash; '); ?>  <?php comments_popup_link('No comments', '1 comment', '% comments'); ?></span><br/>
+						<small><span class="date"><?php the_time('d') ?></span><br /><?php the_time('M y') ?> <!-- by <?php the_author() ?> --></small>
+						<h2><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+						
+						
 
-					<div class="entry">
-						<?php the_content('<em>Continue reading &rarr;</em>'); ?>
+						<div class="entry">
+							<?php the_content('<em>Continue reading &rarr;</em>'); ?>
+						</div>
+						<div class="clearfix"></div>
+
 					</div>
+
+				<?php endwhile; ?>
+
+				<div class="navigation">
+					<div class="alignleft"><?php next_posts_link('&larr; Older Entries') ?></div>
+					<div class="alignright"><?php previous_posts_link('Newer Entries &rarr;') ?></div>
 					<div class="clearfix"></div>
-
 				</div>
-
-			<?php endwhile; ?>
-
-			<div class="navigation">
-				<div class="alignleft"><?php next_posts_link('&larr; Older Entries') ?></div>
-				<div class="alignright"><?php previous_posts_link('Newer Entries &rarr;') ?></div>
-				<div class="clearfix"></div>
-			</div>
-		
-		
+				
+				
 
 			<?php else : ?>
 
